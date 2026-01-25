b7c4637fe0f0d1146eca42c6f9d8358be564cff9
tasu1052 <tasu1052@naver.com>
2026-01-25 21:29:12 +0900

    First Commit

---
+++ src/test/java/com/example/smartParking/SmartParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartParking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
